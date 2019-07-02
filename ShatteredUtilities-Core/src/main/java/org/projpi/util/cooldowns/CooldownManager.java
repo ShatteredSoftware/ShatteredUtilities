e79ed5678e4be81bfd870a980ba16146ee6e9e72
UberPilot <uberpilot99@gmail.com>
2019-07-01 23:03:02 -0600

    Update to 1.3.1: + Add additional logic to CooldownManager. + Document CooldownManager.

--- ShatteredUtilities-Core/src/main/java/org/projpi/util/cooldowns/CooldownManager.java
+++ ShatteredUtilities-Core/src/main/java/org/projpi/util/cooldowns/CooldownManager.java
@@ -8,27 +8,63 @@ public class CooldownManager
 {
     public static final int DEFAULT_COOLDOWN = 1000; // 1000 millis = 1 second
     private final Map<UUID, Long> lastUse;
+    private final int cooldown;
+
+    public CooldownManager()
+    {
+        this(DEFAULT_COOLDOWN);
+    }
 
     public CooldownManager(Integer cooldown)
     {
         lastUse = new HashMap<>();
         if(cooldown == null)
         {
-            cooldown = DEFAULT_COOLDOWN;
+            this.cooldown = DEFAULT_COOLDOWN;
+        }
+        else
+        {
+            this.cooldown = cooldown;
         }
     }
 
+    /**
+     * Checks if a player has time left on this cooldown.
+     *
+     * @param uuid The player to check.
+     * @return Whether this player has time left on their cooldown.
+     */
     public boolean canUse(UUID uuid)
     {
         if(!lastUse.containsKey(uuid))
         {
             return true;
         }
-        return System.currentTimeMillis() > (lastUse.get(uuid) + 1000);
+        return System.currentTimeMillis() > (lastUse.get(uuid) + cooldown);
     }
 
+    /**
+     * Sets last use of this cooldown to the current time for the given player.
+     *
+     * @param uuid The player to set the cooldown for.
+     */
     public void use(UUID uuid)
     {
         lastUse.put(uuid, System.currentTimeMillis());
+    }
+
+    /**
+     * Gets the amount of time left on a cooldown.
+     *
+     * @param uuid The UUID of the player to check.
+     * @return The amount of time left on this cooldown, or 0 if there is none.
+     */
+    public long timeUntilUse(UUID uuid)
+    {
+        if(!lastUse.containsKey(uuid))
+        {
+            return 0;
+        }
+        else return (lastUse.get(uuid) + cooldown) - System.currentTimeMillis();
     }
 }
