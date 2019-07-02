ae77cddf05ad8e274120a6a50d54ee20c22a72e9
UberPilot <uberpilot99@gmail.com>
2019-07-02 00:52:37 -0600

    Add custom model data handling.

--- ShatteredUtilities-1.13.2/src/main/java/org/projpi/util/nms/MaterialUtil13.java
+++ ShatteredUtilities-1.13.2/src/main/java/org/projpi/util/nms/MaterialUtil13.java
@@ -1,10 +1,17 @@
 package org.projpi.util.nms;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.projpi.util.MaterialUtil;
 
 public class MaterialUtil13 implements MaterialUtil
 {
+    @Override
+    public ItemStack setCustomModelData(ItemStack stack, int data)
+    {
+        return stack;
+    }
+
     @Override
     public Material matchMaterial(String name)
     {
--- ShatteredUtilities-Core/src/main/java/org/projpi/util/MaterialUtil.java
+++ ShatteredUtilities-Core/src/main/java/org/projpi/util/MaterialUtil.java
@@ -1,8 +1,10 @@
 package org.projpi.util;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public interface MaterialUtil
 {
     Material matchMaterial(String name);
+    ItemStack setCustomModelData(ItemStack stack, int data);
 }
